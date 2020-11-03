c79a0e8ab3cafa38e57ce0eac9aeb7024340c844
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-11-03 16:21:29 -0700

    My classmates are geniuses and I cannot spell

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,7 +18,7 @@ router.get("/", function (req, res) {
 
 router.post("/api/burgers", function (req, res) {
   burger.create(
-    ["burger_name", "devour"],
+    ["burger_name", "devoured"],
     [req.body.burger_name, req.body.devour],
     function (result) {
       // Send back the ID of the new quote
