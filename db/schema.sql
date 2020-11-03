91c0fd2a63ded286fc84a342a63d78199130cfd2
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-11-02 20:09:11 -0700

    Corrected a typo

--- db/schema.sql
+++ db/schema.sql
@@ -7,5 +7,5 @@ USE burgers_db;
 CREATE TABLE burgers (
   id INT AUTO_INCREMENT PRIMARY KEY,
   burger_name VARCHAR(40) NOT NULL,
-  devoured BOOLEAN DEFAULT flase
+  devoured BOOLEAN DEFAULT false
 );
